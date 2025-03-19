524bd68b545c79f785a281881aa7616df2c9b8f5
Hicham Essaydi <hicham.essaydi@sofrecom.com>
2025-03-19 12:33:16 +0000

    fix: formation and linting issues

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-# This line of code is importing specific functions `multiply`, `add`, and `hello` from the `main`
-# module within the `yt_uv` package. This allows you to use these functions in your current Python
-# script without having to reference the full module path each time you want to use them.
-
-# from yt_uv.main import multiply, add, hello
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
-# from yt_uv import add, multiply
-from yt_uv import *
+from yt_uv import add, multiply
+
+# from yt_uv import *
+
 
 def test_add():
     assert add(1, 2) == 3
