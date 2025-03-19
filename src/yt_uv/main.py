a088c44da1728b6a7134ff0b1d81083acec7284f
Hicham Essaydi <hessaydi@gmail.com>
2025-03-19 13:08:31 +0000

    added: docstring infos to src/main script

--- src/yt_uv/main.py
+++ src/yt_uv/main.py
@@ -1,3 +1,17 @@
+"""
+Author: Hicham Essaydi
+Email: hessaydi@gmail.com
+Date: 2023-10-05
+License: MIT License
+
+Description:
+This module provides functionality for [brief description of what the file does].
+
+Usage:
+[Provide a brief example of how to use the module, if applicable]
+
+"""
+
 def hello() -> str:
     return "Hello from yt-uv!"
 
